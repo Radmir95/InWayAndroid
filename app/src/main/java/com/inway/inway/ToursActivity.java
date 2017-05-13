e0826074f5b562ed1a7f7e8fc75a9b1d3ffbc5b3
khusnutdinov radmir <xtreme95@yandex.ru>
2017-05-13 18:14:39 +0300

    add new activity(fix..)

---
+++ app/src/main/java/com/inway/inway/ToursActivity.java
@@ -0,0 +1,13 @@
+package com.inway.inway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToursActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tours);
+    }
+}
