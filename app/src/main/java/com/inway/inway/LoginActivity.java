d67323bfbd88cba35945e64123d8cd766ea9eba5
Husnutdinov Radmir <XTreme95@yandex.ru>
2017-05-30 08:44:05 +0300

    add new activities

--- app/src/main/java/com/inway/inway/LoginActivity.java
+++ app/src/main/java/com/inway/inway/LoginActivity.java
@@ -3,6 +3,8 @@ package com.inway.inway;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -49,10 +51,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final int REQUEST_READ_CONTACTS = 0;
     private final static String EMPLOYEE_SERVICE_URI = "http://inwayservice.azurewebsites.net/BusDriverLogin.svc/TryLogin/";
 
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-
     private BusDriver driver = null;
 
     private UserLoginTask mAuthTask = null;
@@ -84,6 +82,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
+
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -95,15 +94,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
 
-       // new RetrieveBusDriver().execute();
+    }
 
-//                tvEmployeeCode.setText("Code: " + employee.getString("EmployeeId"));
-//                tvName.setText("Name: " + employee.getString("FirstName") + " " + employee.getString("LastName"));
-//                tvAddress.setText("Address: " + employee.getString("Address"));
-//                tvBloodGroup.setText("Blood Group: " + employee.getString("BloodGroup"));
+    public void startNextActivity(){
 
+        Intent intent = new Intent(this, PassengerActivity.class);
 
-
+        startActivity(intent);
 
     }
 
@@ -198,23 +195,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return true;
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
         return true;
     }
 
-    /**
-     * Shows the progress UI and hides the login form.
-     */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
@@ -297,7 +286,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         int IS_PRIMARY = 1;
     }
 
-
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
@@ -310,24 +298,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-
-
-            try {
-
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-
             return true;
         }
 
@@ -350,8 +320,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
         }
     }
-
-    //TODO: ShowProgress motherfucker yeah!
 
     public class RetrieveBusDriver extends AsyncTask<Void, Void, BusDriver> {
 
@@ -410,14 +378,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (busDriver.busDriverId != -1) {
                 finish();
+
+                startNextActivity();
+
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
             }
-
-            //driver = busDriver;
-
-            //mPasswordView.setText(driver.login);
 
         }
 
